0973b62cca4a8720cf287fd94a20830b029ca1ea
Mohamed Gad <mohamedgad149@gmail.com>
2021-08-19 22:13:22 +0200

    added all the functionality

--- js/app.js
+++ js/app.js
@@ -1,55 +1,82 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
 
-/**
- * Define Global Variables
- * 
-*/
+// Get the sections 
+const sections = document.querySelectorAll("section"); 
+const header = document.querySelector("header"); 
+let liFragment = [] ; 
+// get section titles
+const coordinates = [] ; 
+const titles = []; 
+sections.forEach( (section , index) => {
+    titles[index] = section.getAttribute("data-nav"); 
+    coordinates[index] = section.getBoundingClientRect();
+}); 
+
+// create the nav 
+function createNav(titles){ 
+const ulFrament = document.createElement("ul");
+ulFrament.classList.add("menu");
+liFragment = titles.map((title) => {
+    let li = document.createElement("li");
+    li.textContent = title; 
+    return li ; 
+}); 
+
+liFragment.forEach( (li , i)  => {
+    ulFrament.appendChild(li)
+    li.addEventListener("click" , () => {
+        window.scrollTo({
+            top: Math.ceil(coordinates[i].top),
+            // left: coordinates[i].left,
+            behavior: 'smooth'
+        }); 
+    }); 
+} ); 
+return ulFrament ;
+} 
+
+let setActiveState = () => {
+    let currentScroll = window.scrollY;  
+    
+     
+    coordinates.forEach( (coordinates , index) => {
+        let cooTop = Math.ceil(coordinates.top) ;  
+        let cooBottom = Math.ceil(coordinates.bottom) ;  
+        if(cooTop <= currentScroll && cooBottom > currentScroll ) {
+
+            liFragment.forEach( (li , i) => {
+                if(i == index) {
+                    li.classList.add("active");
+                    // console.log(cooTop + " coordinates.top"  + index); 
+                    // console.log(currentScroll + " currentScroll" + index);     
+                    
+                    sections[i].classList.add("active");
+                }
+                if(i != index && li.classList.contains("active")) {
+                    li.classList.remove("active"); 
+                    sections[i].classList.remove("active");
+
+                }
+            });
+        }
+    } );   
+}
+
+window.addEventListener("scroll" , setActiveState);   
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+header.append(createNav(titles)); 
 
+const menu = document.querySelector("header ul");
+const menuTop = Math.ceil(menu.getBoundingClientRect().y);
 
+window.addEventListener("scroll" , () => {
+    // console.log(menuTop); 
+    if(menuTop <= window.scrollY ) { 
+        menu.classList.add("fixed"); 
+        document.querySelector("body").style.paddingTop = "60px" ; 
+    } else {
+        menu.classList.remove("fixed"); 
+        document.querySelector("body").style.paddingTop = "0" ; 
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
+    }
+});
